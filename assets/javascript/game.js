fdfdddf4e5fa672d3060fd0a03b7cbd5b88ef15c
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-08-13 12:52:03 +1000

    game is looping where guess is incorrect. Need to add the loop to restart the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 var wins = 0;
 var losses = 0;
 var guessesleft = 9;
-var guessedletters;
+var guessedletters =[];
 
 
 var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
@@ -14,37 +14,42 @@ var lossestally = document.getElementById("losses-tally");
 var guesseslefttext = document.getElementById("guesses-tally");
 var guessedletterstext = document.getElementById("guessed-letters");
 
+
+
+  
+
+//Creating the Computer guess, taken from var of the alphabet.
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log("Computer Guess " + computerGuess)
+    
 document.onkeyup = function(event){
 
-    var userGuess = event.key;
+    do{
 
-//taken from the in class activity on RPS. I dont' know what this does yet.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        var userGuess = event.key;
+        guessedletters.push(userGuess);
+          
+            if (userGuess === computerGuess) {
+        wins++;
 
-//checking if the above is working. It is. yay.
-console.log("user guess is " + userGuess);
-console.log("Computer guess is " + computerGuess);
-console.log("Wins: " + wins);
-console.log("Losses: " + losses);
 
- if (userGuess === computerGuess) {
-      wins++
-      
-      ;
-    } else {
-      losses++
-      
-      ;
-    }
+        } else  {
+        guessesleft--;
+        }
 
-    console.log("Wins: " + wins);
-    console.log("Losses: " + losses);   
+ }
 
+    while (guessesleft < 0);
+         
+   
+
+
+//Scorecard
     winstally.textContent = "Wins: " + wins;
     lossestally.textContent = "Losses: " + losses;
     guesseslefttext.textContent = "Guesses Left: " + guessesleft;
-    guessedletterstext.textContent = "Guessed Letters: " + userGuess;
+    guessedletterstext.textContent = "Guessed Letters: " + guessedletters;
     
   
-        }
-    ;
+       
+}
